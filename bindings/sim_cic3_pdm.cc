b43f4afeb172c3ce0a5eb9577135b5c8cf8aa2b1
Jon Nordby <jononor@gmail.com>
2025-08-12 22:17:32 +0200

    Revert back to 3 stage CIC filter
    From 2843105739db84851c3e9d2436c2c3c2c0563abf

--- bindings/sim_cic3_pdm.cc
+++ bindings/sim_cic3_pdm.cc
@@ -15,8 +15,9 @@ pdm2pcm_cic3(const uint8_t *pdm, int64_t pdm_length, int16_t *pcm, int32_t pcm_l
 
 	Vcic3_pdm *top = new Vcic3_pdm{cp};
 
-    top->dc_alpha = hpf_alpha;
-    top->scale_shift = scale_shift;
+    // TODO: bring back high pass filter and adjustable shift
+    //top->dc_alpha = hpf_alpha;
+    //top->scale_shift = scale_shift;
 
 	// Start clock off
 	top->clk = 0;
